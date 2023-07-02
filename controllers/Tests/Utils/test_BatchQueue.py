1fd86e9287fd0b677d55259d4e09cc21d1a063ab
Esk1z0 <j.esteban.rincon.m@gmail.com>
2023-07-02 23:54:02 +0200

    tests passed, maybe the batchqueue will work

--- controllers/Tests/Utils/test_BatchQueue.py
+++ controllers/Tests/Utils/test_BatchQueue.py
@@ -1,12 +1,35 @@
 import unittest
 from controllers.xyz_controller.Utils.BatchQueue import BatchQueue
+from time import sleep
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion
     def test_concurrency(self):
         queue = BatchQueue()
-        queue.dequeue()
+        queue.start()
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+        queue.enqueue(4)
+        queue.enqueue(5)
+
+        print(queue.qsize())
+
+        sleep(0.1)
+        queue.stop()
+        sleep(0.3)
+
+        print(queue.qsize())
+        self.assertEqual(queue.qsize(), 1)
+    def test_run_stop(self):
+        queue = BatchQueue()
+        queue.start()
+        sleep(0.1)
+        print("a")
+        queue.stop()
+        sleep(0.3)
         self.assertEqual(True, True)
 
 
--- controllers/xyz_controller/Utils/BatchQueue.py
+++ controllers/xyz_controller/Utils/BatchQueue.py
@@ -1,3 +1,5 @@
+import threading
+
 from .IBatch import IBatch
 from queue import Queue
 from threading import Thread
@@ -10,19 +12,31 @@ class BatchQueue(Thread, IBatch):
         after each time interval to keep the latest info for the receiver"""
 
     def __int__(self, size: int, interval: int):
-        super().__init__(self, target=self.run())
         self.__queue = Queue(size)
         self.__interval = interval
+        self.__stop = threading.Event()
+        super().__init__(target=self.run())
+        #self.start()
+
     def __init__(self):
-        self.__queue = Queue(1)
-        self.__interval = 0
-        super().__init__(self, target=self.run())
+        self.__queue = Queue(2)
+        self.__interval = 0.1
+        self.__stop = threading.Event()
+        self.__thread = super().__init__()
 
     def run(self):
-        while True:
+        print("running")
+        while not self.__stop.is_set():
+            print("still running")
             sleep(self.__interval)
             if self.__queue.qsize() > 1:
+                print("q>1")
                 self.__queue.get()
+        print("end")
+
+    def stop(self):
+        print("stopping")
+        self.__stop.set()
 
     @property
     def interval(self):
@@ -37,3 +51,6 @@ class BatchQueue(Thread, IBatch):
 
     def enqueue(self, item):
         self.__queue.put(item)
+
+    def qsize(self):
+        return self.__queue.qsize()
