ad7ff68f4860b960c326097e012178c6b7a520d5
Esk1z0 <j.esteban.rincon.m@gmail.com>
2023-07-17 21:54:30 +0200

    distance and imu processes done, angular acceleration no tin use by now

--- controllers/xyz_controller/processes/imu_process.py
+++ controllers/xyz_controller/processes/imu_process.py
@@ -1,7 +1,66 @@
 from processes.iprocess import IProcess
-
+from utils.EInputs import EInput
+import numpy as np
+from scipy.spatial.transform import Rotation
 
 class IMUProcess(IProcess):
 
+    MAX_ROTATION = 7
+    MIN_ROTATION = -7
+
+
     def process_data(self, data):
+        values = data.get(EInput.IMUPosition, 0)
+        interval = data.get(EInput.TimeInterval, 0)
+
+        pos = 0
+        ang_vel = 0
+        #ang_accel = 0
+
+        if (values != 0) and (interval != 0):
+            pos = IMUProcess.rotation_scaler(IMUProcess.get_pos(values, interval))
+            ang_vel = IMUProcess.rotation_scaler(IMUProcess.get_ang_vel(values, interval))
+            #ang_accel = IMUProcess.rotation_scaler(IMUProcess.get_ang_accel(values, interval))
+
+
+        return {EInput.IMUPosition : pos,
+                EInput.IMUAngularVel : ang_vel}
+            #,EInput.IMUAngularAccel : ang_accel}
+
+    @staticmethod
+    def get_pos(values, interval):
+        return values[-1]
+
+    @staticmethod
+    def get_ang_vel(values, interval):
+        q_1 = np.array(values[0])
+        q_2 = np.array(values[-1])
+
+        delta_q = q_1 * np.conjugate(q_2)
+        delta_q /= np.linalg.norm(delta_q)
+        delta_rotation = Rotation.from_quat(delta_q)
+
+        rotation = delta_rotation.as_rotvec() / interval
+
+        return rotation.to_list()
+
+
+    @staticmethod
+    def get_ang_accel(values, interval):
         pass
+
+    @staticmethod
+    def min_max_scaler(data, min_value, max_value):
+        scaled_data = (data - min_value) / (max_value - min_value)
+        if scaled_data > 1:
+            scaled_data = 1
+        elif scaled_data < 0:
+            scaled_data = 0
+        return scaled_data
+
+    @staticmethod
+    def rotation_scaler(rotation):
+        for i in rotation:
+            rotation[rotation.index(i)] = IMUProcess.min_max_scaler(i,
+                                        IMUProcess.MIN_ROTATION, IMUProcess.MAX_ROTATION)
+        return rotation
